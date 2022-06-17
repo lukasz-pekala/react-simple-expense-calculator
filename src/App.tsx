632d051799c18b68e0d9d2786993b398ddcfd2b2
Łukasz Pękala <lukasz-pekala@users.noreply.github.com>
2022-06-17 15:56:29 +0200

    Refactor moved data source to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,9 @@
 import { ThemeProvider } from "styled-components";
 import AddExpenseForm from "./AddExpense/AddExpenseForm";
 import BalanceList from "./BalanceList/BalanceList";
-import { BalanceItem } from "./models/BalanceItem";
 import { StyledHeader } from "./styles/Header.styled";
 import Container from "./styles/Container.styled";
 import GlobalStyles from "./styles/Global";
-
-const balanceItems: BalanceItem[] = [
-  {
-    id: 1,
-    name: "Rent",
-    amount: 1000,
-    type: "expense",
-    category: "Home",
-  },
-  {
-    id: 2,
-    name: "Coffee",
-    amount: 2,
-    type: "expense",
-    category: "Food",
-  },
-  {
-    id: 3,
-    name: "Gas",
-    amount: 100,
-    type: "expense",
-    category: "Transport",
-  },
-  {
-    id: 4,
-    name: "Burrito",
-    amount: 10,
-    type: "expense",
-    category: "Food",
-  },
-  {
-    id: 5,
-    name: "Salary",
-    amount: 10000,
-    type: "income",
-    category: "Work",
-  },
-];
 import { FlatuicolorsSpain } from "./styles/Theme";
 import { BalanceItems } from "./data/BalanceItems";
 
---
+++ src/data/BalanceItems.ts
@@ -0,0 +1,39 @@
+import { BalanceItem } from "../models/BalanceItem";
+
+export const BalanceItems: BalanceItem[] = [
+  {
+    id: 1,
+    name: "Rent",
+    amount: 1000,
+    type: "expense",
+    category: "Home",
+  },
+  {
+    id: 2,
+    name: "Coffee",
+    amount: 2,
+    type: "expense",
+    category: "Food",
+  },
+  {
+    id: 3,
+    name: "Gas",
+    amount: 100,
+    type: "expense",
+    category: "Transport",
+  },
+  {
+    id: 4,
+    name: "Burrito",
+    amount: 10,
+    type: "expense",
+    category: "Food",
+  },
+  {
+    id: 5,
+    name: "Salary",
+    amount: 10000,
+    type: "income",
+    category: "Work",
+  },
+];
